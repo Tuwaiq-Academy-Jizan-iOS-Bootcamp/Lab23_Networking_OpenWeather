ffbe405e6284dd5cddd6f099b4cc577820c1fc66
Hanan Somily <hanan.somily.jiz.s@tuwaiq.edu.sa>
2021-11-30 21:02:30 +0300

    update

--- openWeather/openWeather/ViewController.swift
+++ openWeather/openWeather/ViewController.swift
@@ -14,54 +14,63 @@ class ViewController: UIViewController {
     @IBOutlet weak var coludImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        getData(with: "jazan")
-       // nameCityLabel.text = "jazan"
+
+        insertNameCityTextField.delegate = self
     }
+    @IBAction func searchBotten(_ sender: UIButton) {
+        let cityName = insertNameCityTextField.text
+            getData(with: cityName!)
+    }
+
     func getData(with city:String){
-        
-        let link = " https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=f6ce6c8f34260d59dc97216fd711822d"
-        print(link)
+       // city = cityName!
+        print(city)
+       let link = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=f6ce6c8f34260d59dc97216fd711822d&units=metric"
+//        request(whith: link)
+//        print(link) }
+//        func request(whith link : String){
+print(link)
         if let url = URL(string: link) {
-            print("WAS THE URL IN CORRECT FORMAT")
+            print("WAS THE URL IN CORRECT FORMAT", link)
 
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url) { data , response, error in
+            let task = session.dataTask(with: url) {  data , response, error  in
                 if let error = error {
                     print("ERROR",error.localizedDescription)
                 }else {
-                    print("DO WE HAVE DATA",data)
+                  //  print("DO WE HAVE DATA",data)
                     if let safeData = data {
                         do {
                             let decoder = JSONDecoder()
                             let decodedData = try decoder.decode(WeatherData.self, from: safeData)
+                            print("DECODED DATA",decodedData)
 
                             DispatchQueue.main.async {
             self.nameCityLabel.text = decodedData.name
                                 print(decodedData.name)
-            self.temperaterLabel.text = String( decodedData.coord.lat )
-                                print(decodedData.coord.lat)
+            self.temperaterLabel.text = String(format:"%.0f", decodedData.main.temp )
+                                print(decodedData.main.temp)
                             }
-                            print("DECODED DATA",decodedData)
                         }catch{
                             print("SOMETHING WENT WRONG",error.localizedDescription)
-                DispatchQueue.global().async {
-                    DispatchQueue.main.async {
-                              self.nameCityLabel.text = "invalid data"
-                        }
-                      }
                     }
                   }
                 }
             }
         task.resume()
     }
-    }
-    @IBAction func searchBotten(_ sender: UIButton) {
-    let cityName = insertNameCityTextField.text
-            getData(with: cityName!)
-           // nameCityLabel.text = cityName
-    }
-    
 }
-
+}
+//    @IBAction func searchBotten(_ sender: UIButton) {
+//        if let city = insertNameCityTextField.text {
+//            getData(with: city)
+//           // nameCityLabel.text = cityName
+//    }
+//  }
+//}
+extension ViewController:UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
--- openWeather/openWeather/WitherModel.swift
+++ openWeather/openWeather/WitherModel.swift
@@ -7,17 +7,17 @@
 
 import Foundation
 struct WeatherData:Codable{
-    var coord:Coord
-    var weather:[Weather]
-    var base :String
-    var main : Main
+   var coord:Coord
+   var weather:[Weather]
+   var base :String
+   var main : Main
     var visibility:Int
-    var wind : Wind
-    var clouds :Clouds
-    var dt:Int
-    var sys:Sys
-    var timezone :Int
-    var id :Int
+   var wind : Wind
+//   var clouds :Clouds
+//    var dt:Int
+//    var sys:Sys
+//    var timezone :Int
+   var id :Int
     var name :String
     var cod:Int
 }
@@ -45,7 +45,7 @@ struct Main:Codable{
 }
 
 struct Wind:Codable{
-    var speed: Int
+    var speed:Double
     var deg: Int
     var gust :Double
 }
